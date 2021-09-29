21d3387cd5c5b040d2988253afdee9666a05ce5b
ambuj.chaudhary <ambuj.chaudhary@highradius.com>
2021-09-29 19:42:45 +0530

    Updates

---
+++ precache-manifest.2cf1b89912d84d470966f495182b4bbf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed51f1f6bf277bf11b99ba1dece7ba07",
+    "url": "/blind-chat/index.html"
+  },
+  {
+    "revision": "61b4c19e6b5cb25350f3",
+    "url": "/blind-chat/static/css/main.03f8cf29.chunk.css"
+  },
+  {
+    "revision": "4c01c3aa5590c687d519",
+    "url": "/blind-chat/static/js/2.2462a9f4.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/blind-chat/static/js/2.2462a9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f85410814b78a94a8b5",
+    "url": "/blind-chat/static/js/3.5a5cef09.chunk.js"
+  },
+  {
+    "revision": "61b4c19e6b5cb25350f3",
+    "url": "/blind-chat/static/js/main.d52f7a50.chunk.js"
+  },
+  {
+    "revision": "ca6cec7ee0aa5614bd56",
+    "url": "/blind-chat/static/js/runtime-main.e8582f7f.js"
+  },
+  {
+    "revision": "ee46ca53f7fd77b32d4294985f1bd1be",
+    "url": "/blind-chat/static/media/delete.ee46ca53.svg"
+  }
+]);
